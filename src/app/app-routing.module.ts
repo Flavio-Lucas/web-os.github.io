2100468f9ef050e1c179b33a3caf326f07db4162
Flavio Lucas <flavio.silva@facens.br>
2022-04-11 20:04:45 -0300

    feat(login): added layout for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'desktop', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'desktop', loadChildren: () => import('./pages/desktop/desktop.module').then(m => m.DesktopPageModule) },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
 ];
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     IonicModule,
     LoginPageRoutingModule
   ],
